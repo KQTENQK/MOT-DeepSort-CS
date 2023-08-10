617f62e68f17f5d493d40b540b0393bd64458008
Pavel <xsx3xx@gmail.com>
2023-08-10 16:19:53 +0300

    - Added command line options and console running ability

---
+++ src/MOT/CommandLineOptions.cs
@@ -0,0 +1,74 @@
+﻿using CommandLine;
+
+namespace MOT
+{
+    public sealed class CommandLineOptions
+    {
+        [Option('s', "source", HelpText = "Source video file path.", Required = true)]
+        public string? SourceFilePath { get; set; }
+
+        [Option('t', "target", HelpText = "Target video file path.", Required = true)]
+        public string? TargetFilePath { get; set; }
+
+        [Option('d', "detector", HelpText = "Detector net file path.", Required = true)]
+        public string? DetectorFilePath { get; set; }
+
+        [Option('m', "matcher", HelpText = "Matcher type:\n\t0 for DeepSort\n\t1 for Sort\n\t2 for Deep", Required = true)]
+        public MatcherType? MatcherType { get; set; }
+
+        [Option('y', "yver", HelpText = "Yolo model:\n\t0 for 1280 resolution\n\t1 for 640 resolution", Required = true)]
+        public YoloVersion? YoloVersion { get; set; }
+
+        [Option('v', "aver", HelpText = "Appearance model:\n\t0 for OSNet\n\t1 Fast-Reid", Required = false)]
+        public AppearanceExtractorVersion? AppearanceExtractorVersion { get; set; }
+
+        [Option('a', "appearance", HelpText = "Appearance extractor net file path.", Required = false)]
+        public string? AppearanceExtractorFilePath { get; set; }
+
+        [Option("fps", HelpText = "Target video fps.", Required = false)]
+        public float? FPS { get; set; }
+
+        [Option("threshold", HelpText = "Defines treshold for matcher.", Required = false)]
+        public float? Threshold { get; set; }
+
+        [Option("aweight", HelpText = "Defines appearance weight for deepsort matcher.", Required = false)]
+        public float? AppearanceWeight { get; set; }
+
+        [Option("asmooth", HelpText = "Defines appearance smooth weight for deepsort matcher.", Required = false)]
+        public float? SmoothAppearanceWeight { get; set; }
+
+        [Option("streak", HelpText = "Defines min streak to reidentify person.", Required = false)]
+        public int? MinStreak { get; set; }
+
+        [Option("misses", HelpText = "Defines max misses to lose indentification.", Required = false)]
+        public int? MaxMisses { get; set; }
+
+        [Option("fsmooth", HelpText = "Defines passed frames for smooth weight to be applied.", Required = false)]
+        public int? FramesToAppearanceSmooth { get; set; }
+
+        [Option("acount", HelpText = "Defines appearance extractors in memory count.", Required = false)]
+        public int? ExtractorsInMemoryCount { get; set; }
+
+        [Option('a', "conf", HelpText = "Defines target people detection confidence([0-1]).", Required = false)]
+        public float? TargetConfidence { get; set; }
+    }
+
+    public enum MatcherType : byte
+    {
+        DeepSort = 0,
+        Sort,
+        Deep
+    }
+
+    public enum YoloVersion : byte
+    {
+        Yolo1280 = 0,
+        Yolo640
+    }
+
+    public enum AppearanceExtractorVersion : byte
+    {
+        OSNet = 0,
+        FastReid
+    }
+}
--- src/MOT/Program.cs
+++ src/MOT/Program.cs
@@ -9,28 +9,43 @@ using MOT.CORE.YOLO.Models;
 using System.Drawing;
 using MOT.CORE.Matchers.SORT;
 using MOT.CORE.Matchers.Deep;
+using CommandLine;
+using MOT.CORE.ReID.Models.Fast_Reid;
 
 namespace MOT
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            VideoCapture videoCapture = new VideoCapture("../../../Assets/Input/3.mp4");
+            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            CommandLineOptions options = result.Value;
 
-            double targetFps = videoCapture.Get(Emgu.CV.CvEnum.CapProp.Fps);
+            if (options is null ||
+                string.IsNullOrEmpty(options.SourceFilePath) ||
+                string.IsNullOrEmpty(options.TargetFilePath) ||
+                string.IsNullOrEmpty(options.DetectorFilePath) ||
+                options.MatcherType is null ||
+                options.YoloVersion is null)
+            {
+                Console.WriteLine("Required arguments were not passed.");
+
+                return;
+            }
+
+            VideoCapture videoCapture = new VideoCapture(options.SourceFilePath);
+
+            double targetFps = options.FPS ?? videoCapture.Get(Emgu.CV.CvEnum.CapProp.Fps);
             int width = videoCapture.Width;
             int height = videoCapture.Height;
 
-            VideoWriter videoWriter = new VideoWriter("../../../Assets/Output/test3.mp4", -1, targetFps, new Size(width, height), true);
+            VideoWriter videoWriter = new VideoWriter(options.TargetFilePath, -1, targetFps, new Size(width, height), true);
 
-            var predictor = new YoloScorer<Yolo640v5>(File.ReadAllBytes("../../../Assets/Models/Yolo/yolo640v5.onnx"));
-            var appearanceExtractor = new ReidScorer<OSNet_x1_0>(File.ReadAllBytes("../../../Assets/Models/Reid/osnet_x1_0_msmt17.onnx"), 3);
-
-            var matcher = new DeepSortMatcher(predictor, appearanceExtractor);
-            float targetConfidence = 0.4f;
+            Matcher matcher = ConstructMatcherFromOptions(options);
+            float targetConfidence = float.Clamp(options.TargetConfidence ?? 0.0f, 0.0f, 1.0f);
 
             Mat readBuffer = new Mat();
+
             videoCapture.Read(readBuffer);
 
             while (readBuffer.IsEmpty == false)
@@ -45,9 +60,77 @@ namespace MOT
                 videoCapture.Read(readBuffer);
             }
 
-            predictor.Dispose();
-            appearanceExtractor.Dispose();
+            matcher.Dispose();
             videoWriter.Dispose();
+        }
+
+        private static Matcher ConstructMatcherFromOptions(CommandLineOptions options)
+        {
+            IPredictor predictor = ConstructPredictorFromOptions(options);
+
+            Matcher matcher = options.MatcherType switch
+            { 
+                MatcherType.DeepSort => new DeepSortMatcher(predictor,
+                    ConstructAppearanceExtractorFromOptions(options),
+                    options.AppearanceWeight ?? 0.775f,
+                    options.Threshold ?? 0.5f,
+                    options.MaxMisses ?? 50,
+                    options.FramesToAppearanceSmooth ?? 40,
+                    options.SmoothAppearanceWeight ?? 0.875f,
+                    options.MinStreak ?? 8),
+
+                MatcherType.Sort => new SortMatcher(predictor,
+                    options.Threshold ?? 0.3f,
+                    options.MaxMisses ?? 15,
+                    options.MinStreak ?? 3),
+
+                MatcherType.Deep => new DeepMatcher(predictor,
+                    ConstructAppearanceExtractorFromOptions(options),
+                    options.Threshold ?? 0.875f,
+                    options.MaxMisses ?? 10,
+                    options.MinStreak ?? 4),
+
+                _ => throw new Exception("Matcher cannot be constructed.")
+            };
+
+            return matcher;
+        }
+
+        private static IPredictor ConstructPredictorFromOptions(CommandLineOptions options)
+        {
+            if (string.IsNullOrEmpty(options.DetectorFilePath))
+                throw new ArgumentNullException($"{nameof(options.DetectorFilePath)} was undefined.");
+
+            IPredictor predictor = options.YoloVersion switch
+            {
+                YoloVersion.Yolo640 => new YoloScorer<Yolo640v5>(File.ReadAllBytes(options.DetectorFilePath)),
+                YoloVersion.Yolo1280 => new YoloScorer<Yolo1280v5>(File.ReadAllBytes(options.DetectorFilePath)),
+                _ => throw new Exception("Yolo predictor cannot be constructed.")
+            };
+
+            return predictor;
+        }
+
+        private static IAppearanceExtractor ConstructAppearanceExtractorFromOptions(CommandLineOptions options)
+        {
+            if (string.IsNullOrEmpty(options.AppearanceExtractorFilePath))
+                throw new ArgumentNullException($"{nameof(options.AppearanceExtractorFilePath)} was undefined.");
+
+            if (options.AppearanceExtractorVersion == null)
+                throw new ArgumentNullException($"{nameof(options.AppearanceExtractorVersion)} was undefined.");
+
+            const int DefaultExtractorsCount = 4;
+
+            IAppearanceExtractor appearanceExtractor = options.AppearanceExtractorVersion switch
+            {
+                AppearanceExtractorVersion.OSNet => new ReidScorer<OSNet_x1_0>(File.ReadAllBytes(options.AppearanceExtractorFilePath),
+                    options.ExtractorsInMemoryCount ?? DefaultExtractorsCount),
+                AppearanceExtractorVersion.FastReid => new ReidScorer<Fast_Reid_mobilenetv2>(File.ReadAllBytes(options.AppearanceExtractorFilePath),
+                    options.ExtractorsInMemoryCount ?? DefaultExtractorsCount),
+                _ => throw new Exception("Appearance extractor cannot be constructed.")
+            };
+
+            return appearanceExtractor;
         }
 
         private static void DrawTracks(Bitmap frame, IReadOnlyList<ITrack> tracks)
@@ -71,7 +154,7 @@ namespace MOT
                 (float x, float y) = (track.CurrentBoundingBox.X - xNumberIntent, track.CurrentBoundingBox.Y - yBoundingBoxIntent);
 
                 graphics.DrawString($"{track.Id}",
-                    new Font("Consolas", fontSize, GraphicsUnit.Pixel), new SolidBrush(Color.FromArgb((0xFF << 24) | 0xDBDBDB)),
+                    new Font("Consolas", fontSize, GraphicsUnit.Pixel), new SolidBrush(Color.FromArgb((0xFF << 24) | 0xDDDDDD)),
                     new PointF(x, y));
             }
 
