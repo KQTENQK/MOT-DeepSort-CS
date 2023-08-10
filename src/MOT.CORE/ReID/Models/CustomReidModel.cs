d69eaba0523d694f9b2b2c8d0fb7f8a16bcf3a47
Pavel <xsx3xx@gmail.com>
2023-08-10 16:21:19 +0300

    - Added custom models to allow usability of undefined models

---
+++ src/MOT.CORE/ReID/Models/CustomReidModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MOT.CORE.ReID.Models
+{
+    public class CustomReidModel : IReidModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int BatchSize { get; set; }
+        public int Channels { get; set; }
+        public int OutputVectorSize { get; set; }
+        public string[] Outputs { get; set; }
+        public string Input { get; set; }
+    }
+}
---
+++ src/MOT.CORE/YOLO/Models/CustomYoloModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MOT.CORE.YOLO.Models
+{
+    public class CustomYoloModel : IYoloModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Depth { get; set; }
+        public int Dimensions { get; set; }
+        public int[] Strides { get; set; }
+        public int[][][] Anchors { get; set; }
+        public int[] Shapes { get; set; }
+        public float Confidence { get; set; }
+        public float MulConfidence { get; set; }
+        public float Overlap { get; set; }
+        public int Channels { get; set; }
+        public int BatchSize { get; set; }
+        public string[] Outputs { get; set; }
+        public string Input { get; set; }
+    }
+}
